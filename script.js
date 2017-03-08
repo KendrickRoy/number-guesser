ddd6f53bde4a177a168948187dac0e4f1ea6a6ec
Kendrick Roy <roykendrick934@gmail.com>
2017-03-08 22:37:05 +0000

    number guesser

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-<script src="https://code.jquery.com/jquery-2.1.4.js"></script>
+$(document).ready(function(){
+    $("#result").hide();
+    $("#lost").hide();
+    $("#foolish").hide();
+    $("#kush").hide();
+    $("#soccer").hide();
+    $("#tennis").hide();
+    $("button").click(function(){
+       $("#result").hide();
+        $("#lost").hide();
+        $("#foolish").hide();
+        $("#kush").hide();
+        $("#soccer").hide();
+        $("#tennis").hide();
+        var password=$("input").val();
+        var number = parseInt(password);
+        console.log(number);
+        if(number > 5){
+            console.log(number)
+            $("#kush").show();
+        }
+        else if(number < 5){
+            console.log(number);
+            $("#soccer").show();
+        }
+        if(number === 5){
+            console.log(number);
+            $("#result").show();
+        }    
+        else if(password !== number){
+            console.log(number);
+            $("#tennis").show();
+        }
+        
+    });
+    $
+        
+        
+    })
